71c889b7faca325a84a9fc20b11be680b7ce7c13
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-03-07 22:25:37 +0100

    Improve service content

--- app/tpl/services.tpl.php
+++ app/tpl/services.tpl.php
@@ -12,7 +12,14 @@
             <div class="block block1">
                 <div class="floater flex">
                     <div class="box">
-                        Test Text
+                        <h1>Initial Consulting</h1>
+                        <p>If you are unsure if the Orange-Management application can solve your problems and is the right choice for you we do free consultation where we exchange information and give you our oppinion after hearing your thoughts and having a look at your business.</p>
+
+                        <p>If our application is the right choice for you we will also provide a list of module suggestions in order to satisfy your requirements and do a cost caluculation based on these suggestions.</p>
+
+                        <p>Price: Free<p>
+
+                        <button class="large">More</button>
                     </div>
                     <div class="box">
                         <img src="app/img/cloud-purple.svg" width="75%">
@@ -25,14 +32,26 @@
                         <img src="app/img/cloud-purple.svg" width="75%">
                     </div>
                     <div class="box">
-                        Test Text
+                        <h1>Hosting</h1>
+                        <p>We offer you to host the Orange-Management application on our own servers if you don't want to do it yourself. This makes the setup process of our application even easier and you'll have even less things you need to take care of, giving you more time to focus on your own business.</p>
+
+                        <p>Our hosting services are ideal for small- to mid-sized companies by offering different hosting packages depending on your needs.</p>
+
+                        <p>Prices start at: $X.<p>
+
+                        <button class="large">More</button>
                     </div>
                 </div>
             </div>
             <div class="block block3">
                 <div class="floater flex">
                     <div class="box">
-                        Test Text
+                        <h1>Setup</h1>
+                        <p>If you choose to do the hosting of the Orange-Management application on your own servers or PC we can support you live with the setup process if want to by installing the software on your server or PC.</p>
+
+                        <p>Price: $X.<p>
+
+                        <button class="large">More</button>
                     </div>
                     <div class="box">
                         <img src="app/img/cloud-purple.svg" width="75%">
@@ -45,14 +64,24 @@
                         <img src="app/img/cloud-purple.svg" width="75%">
                     </div>
                     <div class="box">
-                        Test Text
+                        <h1>Migration</h1>
+                        <p>In case you already have existing tools and data you would like to transfer to the Orange-Management application we can do this for you so that you don't lose valuable information and don't have to manage multiple applications during the transitory period.</p>
+
+                        <p>Price: Individual. Depends on the amount and tools used previously.<p>
+
+                        <button class="large">More</button>
                     </div>
                 </div>
             </div>
             <div class="block block2">
                 <div class="floater flex">
                     <div class="box">
-                        Test Text
+                        <h1>Support & Maintenance</h1>
+                        <p>We provide email, phone & remote support in case you have questions or problems. Additionally we do annual maintenance for our application which includes checks for potential database and software problems as well as suggestions in case we see potential for improvements in how you configure and use our software.</p>
+
+                        <p>Prices start at: $X annually.<p>
+
+                        <button class="large">More</button>
                     </div>
                     <div class="box">
                         <img src="app/img/cloud-purple.svg" width="75%">
@@ -65,34 +94,24 @@
                         <img src="app/img/cloud-purple.svg" width="75%">
                     </div>
                     <div class="box">
-                        Test Text
+                        <h1>Customization</h1>
+                        <p>Feedback from users is constantly reviewed and integrated in our solutions, nevertheless there are various situations when custom solutions are required for a company. We provide these custom solutions by customizing existing modules to your requirements.</p>
+
+                        <p>Prices: Individually. Depends on scale.<p>
+
+                        <button class="large">More</button>
                     </div>
                 </div>
             </div>
             <div class="block block1">
                 <div class="floater flex">
                     <div class="box">
-                        Test Text
-                    </div>
-                    <div class="box">
-                        <img src="app/img/cloud-purple.svg" width="75%">
-                    </div>
-                </div>
-            </div>
-            <div class="block block2">
-                <div class="floater flex">
-                    <div class="box">
-                        <img src="app/img/cloud-purple.svg" width="75%">
-                    </div>
-                    <div class="box">
-                        Test Text
-                    </div>
-                </div>
-            </div>
-            <div class="block block3">
-                <div class="floater flex">
-                    <div class="box">
-                        Test Text
+                        <h1>Mini Server</h1>
+                        <p>If you are a small- to mid-sized company you maybe don't have a server for dedicated PC for your business software. This is perfectly fine but if you want to make the next step with a dedicated server but want this server located at your own company instead of hosting it on our servers we provide different hardware solutions which we can setup with your application configuration and ship to you. You simply plug it in at your company and are ready to go.</p>
+
+                        <p>Prices start at: $X.<p>
+
+                        <button class="large">More</button>
                     </div>
                     <div class="box">
                         <img src="app/img/cloud-purple.svg" width="75%">
