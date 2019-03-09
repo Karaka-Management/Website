806246f495d91b6d7bef8b11b31e6ce7410e1d59
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-03-09 16:01:35 +0100

    Remove port

--- app/tpl/head.tpl.php
+++ app/tpl/head.tpl.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<base href="<?= \phpOMS\Uri\UriFactory::build('{/base}:{/port}'); ?>/">
+<base href="<?= \phpOMS\Uri\UriFactory::build('{/base}'); ?>/">
 <link rel="shortcut icon" href="<?= \phpOMS\Uri\UriFactory::build('app/img/favicon.ico'); ?>" type="image/x-icon">
 <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 <link rel="stylesheet" href="app/css/global.css">
