f410a5bebbb6e6da1f2f07b554f1c2121a5d7838
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-03-31 00:35:07 +0100

    Add favicon

--- app/tpl/head.tpl.php
+++ app/tpl/head.tpl.php
@@ -4,3 +4,4 @@
 <link rel="shortcut icon" href="<?= \phpOMS\Uri\UriFactory::build('app/img/favicon.ico'); ?>" type="image/x-icon">
 <link href="//fonts.googleapis.com/css?family=Roboto:100,300,300i,400,700,900" rel="stylesheet">
 <link rel="stylesheet" href="app/css/global.css">
+<link rel="shortcut icon" href="app/img/favicon.ico" type="image/x-icon">
