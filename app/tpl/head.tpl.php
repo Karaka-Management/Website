b4cff68ff03e80579f8d1deb2a32d47f0ddbee85
Dennis Eichhorn <spl1nes.com@googlemail.com>
2019-04-03 21:02:58 +0200

    Fix double favicon

--- app/tpl/head.tpl.php
+++ app/tpl/head.tpl.php
@@ -1,7 +1,6 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <base href="<?= \phpOMS\Uri\UriFactory::build('{/base}'); ?>/">
-<link rel="shortcut icon" href="<?= \phpOMS\Uri\UriFactory::build('app/img/favicon.ico'); ?>" type="image/x-icon">
 <link href="//fonts.googleapis.com/css?family=Roboto:100,300,300i,400,700,900" rel="stylesheet">
 <link rel="stylesheet" href="app/css/global.css">
 <link rel="shortcut icon" href="app/img/favicon.ico" type="image/x-icon">
