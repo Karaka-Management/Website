5034e874c157944ac09696a92095503a625d1413
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-07-20 15:47:23 +0200

    Update footer.tpl.php

--- app/tpl/footer.tpl.php
+++ app/tpl/footer.tpl.php
@@ -25,7 +25,6 @@
                     <li><a href="/imprint">Imprint</a>
                     <li><a href="/terms">Terms of Service</a>
                     <li><a href="/privacy">Privacy Policy</a>
-                    <li><a href="/security">Security Statmeent</a>
                 </ul>
             </div>
         </div>
