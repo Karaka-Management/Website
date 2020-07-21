0e1607e9acf64840fdb6aefdd5dcfd81a55cdfc0
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-07-21 20:46:19 +0200

    Update footer.tpl.php

--- app/tpl/footer.tpl.php
+++ app/tpl/footer.tpl.php
@@ -6,8 +6,6 @@
                 <div>
                     <p>Orange-Management was founded in XXX by Dennis Eichhorn and provides smart business solutions for every company size. Managing and performing daily business tasks has naver been easier.</p>
                     <p>Our software is user friendly, performant, affordable and just works.</p>
-                    <p><a href="https://www.patreon.com/bePatron?u=18592707" data-patreon-widget-type="become-patron-button">Become a Patron!</a><script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script></p>
-                    <p><form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top"><input type="hidden" name="cmd" value="_s-xclick" /><input type="hidden" name="hosted_button_id" value="EZ77DXQG85S3Y" /><input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" /></form></p>
                 </div>
             </div>
             <div>
