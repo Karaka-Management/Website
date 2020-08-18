0e9cb0aa185f29838b71584992c04bc7fb9331dd
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-08-18 19:56:18 +0200

    fix capitalization

--- app/tpl/privacy_policy.tpl.php
+++ app/tpl/privacy_policy.tpl.php
@@ -13,7 +13,7 @@
                 <div class="floater">
                     <h1>Privacy Policy</h1>
                     <p>This privacy policy ("POLICY") will help you understand how [name] ("us", "we", "our") uses and protects the data you provide to us when you visit and use Orange-Management ("website", "service" and "application").</p>
-                    
+
                     <h2>Definitions</h2>
                     <p>For the purposes of these POLICIES:<p>
 
@@ -21,15 +21,15 @@
                         <li>AFFILIATED means an entity that controls, is controlled by or is under common control with a party, where "control" means ownership of 50% or more of the shares, equity interest or other securities entitled to vote for election of directors or other managing authority.</li>
                         <li>COUNTRY refers to Germany</li>
                         <li>COMPANY (referred to as either "the Company", "We", "Us" or "Our" in this AGREEMENT) refers to Orange Management, Gartenstr. 26, 61206 Woellstadt.</li>
-                        <li>DEVICE means any device that can access the Service such as a computer, a cellphone or a digital tablet.</li>
+                        <li>DEVICE means any device that can access the SERVICE such as a computer, a cellphone or a digital tablet.</li>
                         <li>SERVICE refers to the Website</li>
                         <li>POLICY or AGREEMENT mean these policies that form the entire agreement between You and the COMPANY regarding the use of the SERVICE.</li>
-                        <li>Third-party Social Media Service means any services or content (including data, information, products or services) provided by a third-party that may be displayed, included or made available by the SERVICE.</li>
+                        <li>Third-party Social Media SERVICE means any services or content (including data, information, products or services) provided by a third-party that may be displayed, included or made available by the SERVICE.</li>
                         <li>WEBSITE refers to orange-management.org (.net, .app, .service, .support, .email, .de, .solutions)</li>
                         <li>APPLICATION refers to all downloadable or installable content which can therfore be used on an a given DEVICE.</li>
-                        <li>You means the individual accessing or using the Service, or the company, or other legal entity on behalf of which such individual is accessing or using the Service, as applicable.</li>
+                        <li>You means the individual accessing or using the SERVICE, or the company, or other legal entity on behalf of which such individual is accessing or using the SERVICE, as applicable.</li>
                     </ul>
-                    
+
                     <h2>What User Data we Collect</h2>
                     <p>When you visit the WEBSITE or APPLICATION, we may collect the following data:</p>
 
@@ -38,7 +38,7 @@
                         <li>Your contact information and email address.</li>
                         <li>Data profile regarding your online behavior on our WEBSITE.</li>
                     </ul>
-                        
+
                     <h2>Why We Collect Your Data</h2>
                     <p>We are collecting your data for several reasons:</p>
 
@@ -49,7 +49,7 @@
                         <li>To contact you to fill out surveys and participate in other types of market research.</li>
                         <li>To customize our WEBSITE according to your online behavior and personal preferences.</li>
                     </ul>
-                    
+
                     <h2>Safeguarding and Securing the Data</h2>
                     <p>Orange-Management is committed to securing your data and keeping it confidential. Orange-Management has done all in its power to prevent data theft, unauthorized access, and disclosure by implementing the latest technologies and software, which help us safeguard all the information we collect online.</p>
 
@@ -66,27 +66,27 @@
 
                     <h2>Restricting the Collection of your Personal Data</h2>
                     <p>At some point, you might wish to restrict the use and collection of your personal data. You can achieve this by doing the following:</p>
-                    
+
                     <ul>
                         <li>Don't accept cookies from our WEBSITE and APPLICATION</li>
                         <li>If you have already agreed to share your information with us, feel free to contact us via email and we will be more than happy to change this for you.</li>
-                    </ul>    
-                    
+                    </ul>
+
                     <p>Orange-Management will not lease, sell or distribute your personal information to any third parties, unless we have your permission. We might do so if the law forces us. Your personal information will be used when we need to send you promotional materials if you agree to this privacy policy.</p>
-                
+
                     <h2>Governing Law</h2>
                     <p>The laws of the COUNTRY, excluding its conflicts of law rules, shall govern this POLICY and Your use of the SERVICE. Your use of the APPLICATION may also be subject to other local, state, national, or international laws.</p>
                     <p>The ineffectiveness of one or more provisions of this agreement does not affect the validity of the others. Each party to these TERMS can in this case demand that a new valid provision be agreed which best achieves the economic purpose of the ineffective provision.</p>
-                
+
                     <h2>Dispute Resolution</h2>
-                    <p>If You have any concern or dispute about the Service, You agree to first try to resolve the dispute informally by contacting the COMPANY.</p>
-                
+                    <p>If You have any concern or dispute about the SERVICE, You agree to first try to resolve the dispute informally by contacting the COMPANY.</p>
+
                     <h2>Changes to these Policies</h2>
                     <p>We reserve the right, at Our sole discretion, to modify or replace these POLICIES at any time. If a revision is material We will make reasonable efforts to provide at least 30 days' notice prior to any new terms taking effect. What constitutes a material change will be determined at Our sole discretion.</p>
                     <p>By continuing to access or use Our SERVICE after those revisions become effective, You agree to be bound by the revised policies. If You do not agree to the new policies, in whole or in part, please stop using the WEBSITE and the SERVICE.</p>
-                
+
                     <h2>Contact</h2>
-                    <p>For questions regarding these PLICIES please feel free to contact us at info@orange-management.email</p>
+                    <p>For questions regarding these POLICIES please feel free to contact us at info@orange-management.email</p>
                 </div>
             </div>
         </main>
