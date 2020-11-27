90c255a06a7e977fef55f16cab625ddc6509f065
Dennis Eichhorn <spl1nes.com@googlemail.com>
2020-11-27 11:56:26 +0100

    start drafting shop

--- app/tpl/shop.tpl.php
+++ app/tpl/shop.tpl.php
@@ -3,15 +3,72 @@
     <head>
         <title>Orange-Management - Shop</title>
         <?php include __DIR__ . '/head.tpl.php'; ?>
-        <link rel="stylesheet" href="app/css/upcoming.css">
+        <link rel="stylesheet" href="app/css/services.css">
     </head>
     <body>
         <header>
             <?php include __DIR__ . '/nav.tpl.php'; ?>
         </header>
-        <main id="upcoming" class="block">
-            <div class="box section">
-                <h1>Coming soon...</h1>
+        <main>
+            <div class="block block2">
+                <div class="floater flex">
+                    <div class="box">
+                    </div>
+                </div>
+            </div>
+            <div class="block block3">
+                <div class="floater">
+                    <div class="flex section">
+                        <div class="box box2">
+                            <div class="tcenter"><img src="app/img/undraw_mind_map_cwng.svg" loading="lazy" width="40%"></div>
+                            <h1>ERP</h1>
+                            <p>In the ERP package we include a billing system for writing invoices, customer, supplier and article management as well as warehousing.</p>
+                        </div>
+
+                        <div class="box box2">
+                            <div class="tcenter"><img src="app/img/undraw_personal_info_0okl.svg" loading="lazy" width="47%"></div>
+                            <h1>CRM</h1>
+                            <p>The CRM package includes analysis tools for your customers and sales. You'll be able to manage customer groups and store additional information such as documents, notes, customer communication etc.</p>
+                        </div>
+
+                        <div class="box box2">
+                            <div class="tcenter"><img src="app/img/undraw_personal_finance_tqcd.svg" loading="lazy" width="27%"></div>
+                            <h1>Finance</h1>
+                            <p>The Finance package allows you to book invoices (receivable/payable) and manage your accounting such as income statement & balance.</p>
+                        </div>
+
+                        <div class="box box2">
+                            <div class="tcenter"><img src="app/img/undraw_noted_pc9f.svg" loading="lazy" width="28%"></div>
+                            <h1>Office</h1>
+                            <p>With the Office package you can create text documents, spreadsheets and create presentations. Further features are messaging (e.g. email) a calender and task management for organizing your work.</p>
+                        </div>
+                    </div>
+                    <div class="flex section">
+                        <div class="box box2">
+                            <div class="tcenter"><img src="app/img/undraw_wall_post_83ul.svg" loading="lazy" width="30%"></div>
+                            <h1>CMS</h1>
+                            <p>Creating and managing a website becomes very easy with the CMS package. We provide tools to create individual pages and manage their content.</p>
+                        </div>
+
+                        <div class="box box2">
+                            <div class="tcenter"><img src="app/img/undraw_financial_data_es63.svg" loading="lazy" width="33%"></div>
+                            <h1>Analytic</h1>
+                            <p>The Analytic package provides detailed information of your business such as detailed finance analysis, customizable reports and your general business activities.</p>
+                        </div>
+
+                        <div class="box box2">
+                            <div class="tcenter"><img src="app/img/undraw_factory_dy0a.svg" loading="lazy" width="40%"></div>
+                            <h1>Production</h1>
+                            <p>With the Production package you can create and manage production processes for production scheduling, manufacturing new products and manage production costs.</p>
+                        </div>
+
+                        <div class="box box2">
+                            <div class="tcenter"><img src="app/img/undraw_personal_file_222m.svg" loading="lazy" width="42%"></div>
+                            <h1>SRM</h1>
+                            <p>The SRM package includes analysis tools for your suppliers and procuring process. You'll be able to manage supplier groups and store additional information such as documents, notes, customer communication etc.</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </main>
         <?php include __DIR__ . '/footer.tpl.php'; ?>
